ad2e1468dc49fd7d1207687f2d00e7ac26fb2e7e
Ian Young <ian@iay.org.uk>
2017-10-09 17:17:49 +0100

    Refactor AcceptStringRegexValidator

--- src/main/java/uk/org/iay/incommon/mda/validate/string/AcceptStringRegexValidator.java
+++ src/main/java/uk/org/iay/incommon/mda/validate/string/AcceptStringRegexValidator.java
@@ -15,16 +15,11 @@
 package uk.org.iay.incommon.mda.validate.string;
 
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import javax.annotation.Nonnull;
 
 import net.shibboleth.metadata.Item;
-import net.shibboleth.metadata.validate.BaseValidator;
 import net.shibboleth.metadata.validate.Validator;
-import net.shibboleth.utilities.java.support.annotation.constraint.NonnullAfterInit;
-import net.shibboleth.utilities.java.support.component.ComponentInitializationException;
-import net.shibboleth.utilities.java.support.component.ComponentSupport;
 
 /**
  * A <code>Validator</code> that accepts {@link String} values matching a regular expression.
@@ -32,56 +27,15 @@ import net.shibboleth.utilities.java.support.component.ComponentSupport;
  * This validator returns {@link net.shibboleth.metadata.validate.Validator.Action#DONE}
  * if the entire value is matched by the regular expression, thus terminating any validator sequence.
  */
-public class AcceptStringRegexValidator extends BaseValidator implements Validator<String> {
-
-    /** Regular expression to be accepted by this validator. */
-    @NonnullAfterInit
-    private String regex;
-
-    /** Compiled regular expression to use in match operations. */
-    @NonnullAfterInit
-    private Pattern pattern;
-
-    /**
-     * Returns the regular expression.
-     *
-     * @return Returns the regular expression.
-     */
-    @NonnullAfterInit
-    public String getRegex() {
-        return regex;
-    }
-
-    /**
-     * Sets the regular expression to be accepted.
-     *
-     * @param r the regular expression to set.
-     */
-    public void setRegex(@Nonnull final String r) {
-        ComponentSupport.ifDestroyedThrowDestroyedComponentException(this);
-        ComponentSupport.ifInitializedThrowUnmodifiabledComponentException(this);
-
-        regex = r;
-    }
+public class AcceptStringRegexValidator extends BaseStringRegexValidator implements Validator<String> {
 
     @Override
     public Action validate(@Nonnull final String e, @Nonnull final Item<?> item, @Nonnull final String stageId) {
-        final Matcher matcher = pattern.matcher(e);
+        final Matcher matcher = getPattern().matcher(e);
         if (matcher.matches()) {
             return Action.DONE;
         } else {
             return Action.CONTINUE;
         }
-    }
-
-    @Override
-    protected void doInitialize() throws ComponentInitializationException {
-        super.doInitialize();
-
-        if (getRegex() == null) {
-            throw new ComponentInitializationException("regular expression to be matched can not be null");
-        }
-
-        pattern = Pattern.compile(regex);
     }
 }
