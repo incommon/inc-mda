8001a010edd7ba7467511b5fd08af740d3cd9011
Ian Young <ian@iay.org.uk>
2017-10-11 17:56:41 +0100

    Add ScopeValidationStage

---
+++ src/main/java/uk/org/iay/incommon/mda/dom/saml/shib/ShibbolethMetadataSupport.java
@@ -0,0 +1,40 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.org.iay.incommon.mda.dom.saml.shib;
+
+import javax.annotation.concurrent.ThreadSafe;
+import javax.xml.namespace.QName;
+
+/** Helper class for dealing with Shibboleth metadata. */
+@ThreadSafe
+public final class ShibbolethMetadataSupport {
+
+    /** Shibboleth metadata namespace URI. */
+    public static final String SHIBMD_NS = "urn:mace:shibboleth:metadata:1.0";
+
+    /** Default Shibboleth metadata namespace prefix. */
+    public static final String SHIBMD_PREFIX = "shibmd";
+
+    /** Scope element name. */
+    public static final QName SCOPE_NAME = new QName(SHIBMD_NS, "Scope", SHIBMD_PREFIX);
+
+    /** regexp attribute name. */
+    public static final QName REGEXP_ATTRIB_NAME = new QName("regexp");
+
+    /** Constructor. */
+    private ShibbolethMetadataSupport() {
+
+    }
+}
